371de91fc70349e2ac7faf20f46a69a9ab994464
usamant-gcp <94663861+usamant-gcp@users.noreply.github.com>
2023-09-03 16:09:01 +0000

    Test Helloworld Java

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test
+{
+    public void HelloWorld() {
+        System.out.println("Export name");
+    }
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+        System.out.println("What is my name?");
+    }
+};
