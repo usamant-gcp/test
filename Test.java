d99532e926cd54d1ba97a2662bb9799e4b02c513
usamant-gcp <94663861+usamant-gcp@users.noreply.github.com>
2023-10-01 04:36:19 +0000

    added ascii print

--- Test.java
+++ Test.java
@@ -1,10 +1,48 @@
-class Test
-{
+class Test {
     public void HelloWorld() {
         System.out.println("Export name");
     }
-    public static void main(String []args)
-    {
-        System.out.println("bc" + 2 + 3);
+
+    public void run() {
+        int numLines = 7;
+        for (int i = 0; i < numLines; ++i) {
+            System.out.println(N[i] + "\t" + A[i] + "\t" + S[i]);
+        }
     }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.run();
+    }
+
+    private String[] N = {
+            "N   N",
+            "NN  N",
+            "N N N",
+            "N  NN",
+            "N   N",
+            "N   N",
+            "N   N"
+    };
+
+    private String[] S = {
+            " SSS ",
+            "S   S",
+            "S    ",
+            " SSS ",
+            "    S",
+            "S   S",
+            " SSS "
+
+    };
+
+    private String[] A = {
+            "  A  ",
+            " A A ",
+            "A   A",
+            "AAAAA",
+            "A   A",
+            "A   A",
+            "A   A"
+    };
 };
